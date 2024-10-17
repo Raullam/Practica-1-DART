85f09872ee522aa5c9debe9d261f2d19f3886f16
Raül Lama <110611752+Raullam@users.noreply.github.com>
2024-10-17 18:26:12 +0200

    Acabat Ex2.dart

--- PRACTICA 1  DART/Ex2.dart
+++ PRACTICA 1  DART/Ex2.dart
@@ -1,40 +1,42 @@
 void main(List<String> args) {
-  if (args.isEmpty) {
+  if (args.isEmpty) { // Si aarguments esta buit enviem l'avis i tanquem el programa
     print('Por favor, introeix un numero com argument.');
     return;
   }
 
-  // Convertir el argumento a entero
-  int n = int.parse(args[0]);
-  
-  // Llamar a la función con el valor obtenido
-  generaPrimers(n);
+  int n = int.parse(args[0]);    // Convertim l'argument 0 que es el que ens interesa a enter
+  generaPrimers(n);              // Cridem a la funció generarPrimers()
 }
 
 void generaPrimers(int n) {
   List<int> numerosPrimos = [];
 
-  // Encontrar y almacenar los números primos en la lista
-  for (int i = 2; i < n; i++) {
-    if (esPrimo(i)) {
-      numerosPrimos.add(i);
+  for (int i = 2; i < n; i++) {   // Busquem els numeros primers mes petits que l'argument ingresat
+    if (esPrimo(i)) {             // Amb el bucle for mirem tots els nombres inferiors a l'argument
+      numerosPrimos.add(i);       // i amb la funció mirem si es primer o no
     }
   }
 
-  // Mostrar los números primos encontrados
-  print("Numeros primos menors de $n:");
+  print("Numeros primos menors de $n:");  // Mostra els numeros primers trobats
   print(numerosPrimos);
 
-  // Encontrar y mostrar las parejas de números primos cuya diferencia sea 2
-  print("Parelles de numeros primos amb diferencia de 2:");
-  for (int i = 0; i < numerosPrimos.length - 1; i++) {
-    if (numerosPrimos[i + 1] - numerosPrimos[i] == 2) {
-      print("(${numerosPrimos[i]}, ${numerosPrimos[i + 1]})");
+  print("Parelles de numeros primos amb diferencia de 2:"); // Busquem i mostrem les parelles de numeros primers que la diferencia entre ells sigue de 2
+  for (int i = 0; i < numerosPrimos.length - 1; i++) {      // 
+    if (numerosPrimos[i + 1] - numerosPrimos[i] == 2) {     // Aquí s'examina si la diferència entre el nombre primer actual (numerosPrimos[i]) i el següent 
+      print("(${numerosPrimos[i]}, ${numerosPrimos[i + 1]})"); //(numerosPrimos[i + 1]) és exactament 2. Si la condició és certa, vol dir que hem trobat una parella de nombres primers que satisfà la condició desitjada.
+    }
+  }
+}
+ print("Parelles de numeros primos amb diferencia de 2:"); // Busquem i mostrem les parelles de numeros primers que la diferencia entre ells sigue de 2.
+  for (int i = 0; i < numerosPrimos.length - 1; i++) {      // Aquí s'examina si la diferència entre el nombre primer actual  és exactament 2.
+    if (numerosPrimos[i + 1] - numerosPrimos[i] == 2) {     //  Si la condició és certa, vol dir que hem trobat una parella de nombres primers
+      print("(${numerosPrimos[i]}, ${numerosPrimos[i + 1]})"); // i l'impirmim
     }
   }
 }
 
-bool esPrimo(int num) {
+
+bool esPrimo(int num) { // Funció que ens diu si un valor int es un numero primer i ens retorna true o false
   if (num < 2) return false;
   for (int i = 2; i * i <= num; i++) {
     if (num % i == 0) return false;
